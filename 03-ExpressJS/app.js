3473d964f51557c8c5ba188cc78efe17f0cdadb4
Soofa <youssefjoo2255@gmail.com>
2026-07-12 17:44:49 +0300

    Parsing the request with express

--- 03-ExpressJS/app.js
+++ 03-ExpressJS/app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 
 const app = express();
+
+
+app.use(bodyParser.urlencoded())
 
 // app.use() registers a middleware function in Express.
 
@@ -11,11 +15,15 @@ app.use('/', (req, res, next) => {
 
 app.use('/add-product',(req, res, next) => {
     console.log("In another middleware!"); 
-    res.send('<h1>Add Product page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.use('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/'); 
+});
 
-app.use('/h',(req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log("In another middleware!"); 
     res.send('<h1>Hello from Express!</h1>');
 });
